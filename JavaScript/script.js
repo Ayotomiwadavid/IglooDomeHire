19337f19913ca82a0bdfeab6f709e96ae43626e3
Techthoth <ayotomiwadavid@proton.me>
2024-07-08 18:07:41 -0700

    saving progress

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,4 +3,13 @@ const sideBar = document.querySelector(".sideBar");
 
 menuToggler.addEventListener("click", () => {
   sideBar.classList.toggle("active");
+
+  // Check if the sidebar has the 'active' class
+  if (sideBar.classList.contains("active")) {
+    // Change the image to 'close-icon.png' when the sidebar is active
+    menuToggler.src = "./images/close.png";
+  } else {
+    // Change the image back to 'menu-icon.png' when the sidebar is not active
+    menuToggler.src = "./images/menu.png";
+  }
 });
